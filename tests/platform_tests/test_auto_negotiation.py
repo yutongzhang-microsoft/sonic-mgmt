2e38f0212d7b40ef5bfa5ab1b69fe484011df325
Yutong Zhang <yutongzhang@microsoft.com>
2025-02-27 10:36:05 +0800

    Create conflict

--- tests/platform_tests/test_auto_negotiation.py
+++ tests/platform_tests/test_auto_negotiation.py
@@ -123,7 +123,7 @@ def recover_ports(duthosts, fanouthosts):
 
     logger.info('Recovering port configuration for DUT...')
     for duthost in duthosts:
-        config_reload(duthost)
+        config_reload(duthost, safe_reload=False)
 
 
 @contextlib.contextmanager
